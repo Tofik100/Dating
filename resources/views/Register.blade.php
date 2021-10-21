abf4c48bab4b217ea5975c6adf8d68ad755198e2
Tofik100 <tofik@coretechies.com>
2021-10-21 11:47:02 +0530

    good one

--- resources/views/Register.blade.php
+++ resources/views/Register.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registration</title>
+    <title>Registration Done</title>
 </head>
 <body>
 
-    <h2>User Register Successfully !!!!</h2>
+    <h2>User Register Successfully</h2>
     
 </body>
 </html>
